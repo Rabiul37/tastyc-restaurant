af53dff3a730598f150758d793ba6ebb1a092ee5
Rabiul37 <hmrabiul37@gmail.com>
2023-11-08 08:49:35 +0600

    blog page completed

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,11 +1,249 @@
 import Navber from "../Components/Navber";
 import PageTitle from "../Components/PageTitle";
-
+import { BsDatabase } from "react-icons/Bs";
+import { SiNpm } from "react-icons/Si";
+import { DiNodejsSmall } from "react-icons/Di";
+import { DiMongodb } from "react-icons/Di";
+import { DiMysql } from "react-icons/Di";
+import data from "../assets/images/data.png";
+import npm from "../assets/images/npm.png";
 const Blog = () => {
   return (
-    <div>
+    <div className="appContainer">
       <PageTitle title="Tastyc-Blog"></PageTitle>
       <Navber></Navber>
+      <div className="mt-10">
+        <div className="h-[1px] -mt-10 w-full outline-none bg-gray-200 text-center mb-5">
+          {" "}
+        </div>
+        <div>
+          <h1 className="flex justify-center gap-5 text-2xl text-center font-semibold lg:text-4xl tracking-wider  text-[#E5EBEF] font-philosopher ml-8">
+            <span>
+              {" "}
+              <BsDatabase></BsDatabase>
+            </span>{" "}
+            <span> One way data binding</span>
+          </h1>
+          <div className="text-1xl mt-4 mb-4 font-semibold font-opensens text-gray-300 w-10/12 m-auto flex flex-col gap-5">
+            <p>
+              One-way data binding is a concept in web development that
+              describes a unidirectional flow of data from a data source to a
+              user interface element, such as a web page or a UI component. In
+              one-way data binding, changes to the data source are reflected in
+              the user interface, but changes in the user interface do not
+              affect the data source. This means that the data source is the
+              single source of truth, and the user interface is updated based on
+              the data sources state.
+            </p>
+            <div className="flex justify-center">
+              <img src={data} alt="" />
+            </div>
+            <p>
+              One-way data binding is commonly used in various web development
+              frameworks and libraries, and it helps ensure a clear and
+              predictable data flow, making it easier to manage the applications
+              state and update the user interface in a consistent manner.
+            </p>
+            <p>
+              For example, in the context of a JavaScript framework like
+              Angular, one-way data binding can be implemented using property
+              binding, where data properties from the component class are bound
+              to elements in the HTML template. When the data in the component
+              changes, the corresponding elements in the template are
+              automatically updated to reflect the new data, but changes made by
+              users in the UI (like form inputs) do not directly modify the
+              underlying data source. This separation of concerns helps maintain
+              data integrity and simplifies data management in complex web
+              applications.
+            </p>
+            <p>
+              In contrast, two-way data binding allows changes in the user
+              interface to also update the data source, creating a bidirectional
+              relationship. Two-way data binding is often used in frameworks
+              like Angular with the ngModel directive or in libraries like React
+              with controlled components to allow for interactive user
+              interfaces where changes in the UI instantly affect the data
+              source.
+            </p>
+          </div>
+        </div>
+        <div className="h-[1px]  w-full outline-none bg-gray-200 text-center mb-5">
+          {" "}
+        </div>
+        {/* npm in  node js */}
+        <div>
+          <div className="flex justify-center gap-5 text-2xl text-center font-semibold lg:text-4xl tracking-wider  text-[#E5EBEF] font-philosopher ml-8">
+            <span>
+              <SiNpm></SiNpm>
+            </span>{" "}
+            <h1>NPM in node.js</h1>
+            <DiNodejsSmall></DiNodejsSmall>
+          </div>
+          <div className="text-1xl mt-4 font-semibold font-opensens text-gray-300 w-10/12 m-auto flex flex-col gap-5">
+            <p>
+              (1) Package Management: NPM is used to install, update, and manage
+              packages and dependencies in Node.js applications. It allows
+              developers to specify the dependencies their project needs in a
+              package.json file, and NPM can then install these dependencies and
+              their sub-dependencies with a single command.
+            </p>
+            <p>
+              (2) Command-Line Interface (CLI): NPM provides a command-line
+              interface that allows developers to interact with it from the
+              terminal. Common NPM commands include npm install to install
+              dependencies, npm start to run a Node.js application, npm publish
+              to publish packages to the NPM registry, and many others.
+            </p>
+            <div className="flex justify-center">
+              <img src={npm} alt="" />
+            </div>
+            <p>
+              (3) Package Registry: NPM maintains a public registry of Node.js
+              packages and modules. Developers can publish their own packages to
+              this registry, making them available to the Node.js community. The
+              NPM registry is a vast collection of open-source packages that can
+              be easily reused in Node.js projects.
+            </p>
+            <p>
+              (4) Semantic Versioning: NPM uses semantic versioning (SemVer) to
+              manage package versions. This helps ensure that updates to
+              dependencies are compatible and do not introduce breaking changes.
+            </p>
+            <p>
+              (5) Dependency Resolution: NPM automatically resolves and installs
+              dependencies, taking care of complex dependency trees, ensuring
+              that packages are compatible with each other, and handling version
+              conflicts.
+            </p>
+            <p>
+              (6) Custom Scripts: NPM allows developers to define custom scripts
+              in the package.json file, which can be executed via the NPM CLI.
+              This is often used for defining build processes, testing, and
+              running other development tasks.
+            </p>
+            <p>
+              (7) Package Lock: NPM generates a package-lock.json file that
+              locks down the specific versions of dependencies used in a
+              project. This ensures that the same versions of dependencies are
+              installed consistently across different development environments.
+            </p>
+            <p>
+              Node Package Manager is an essential tool in the Node.js
+              ecosystem, making it easier for developers to manage dependencies,
+              share their own packages, and streamline the development and
+              deployment process of Node.js applications.
+            </p>
+          </div>
+          <div></div>
+        </div>
+        {/* Mongodb database vs mySQL database */}
+        <div className="h-[1px] mt-8 w-full outline-none bg-gray-200 text-center mb-5">
+          {" "}
+        </div>
+        <div>
+          <div className="flex justify-center gap-5 text-2xl text-center font-semibold lg:text-4xl tracking-wider  text-[#E5EBEF] font-philosopher ml-8">
+            <span>
+              {" "}
+              <DiMongodb></DiMongodb>
+            </span>
+            <h1> Mongodb database vs mySQL database</h1>
+            <span>
+              <DiMysql></DiMysql>
+            </span>
+          </div>
+          <div className="text-1xl mt-4 font-semibold font-opensens text-gray-300 w-10/12 m-auto flex flex-col gap-5">
+            <p>
+              MongoDB and MySQL are both popular database management systems,
+              but they have different data models and are suitable for different
+              use cases. Here are some key differences between MongoDB and
+              MySQL:
+            </p>
+            <h1>
+              <span className="text-xl text-[#ff3811]">1. Data Model</span>
+              <p>
+                {" "}
+                MongoDB - MongoDB is a NoSQL database, which means it uses a
+                document-oriented data model. It stores data in flexible,
+                semi-structured BSON (Binary JSON) documents. Each document can
+                have a different structure, allowing for dynamic and schema-less
+                data.
+              </p>
+              <br />
+              <p>
+                MySQL - MySQL is a relational database management system
+                (RDBMS), which uses a tabular, structured data model. Data is
+                organized into tables with fixed schemas, where each row must
+                adhere to the tables structure.
+              </p>
+            </h1>
+            <h1>
+              <span className="text-xl text-[#ff3811]">
+                {" "}
+                2. Schema Flexibility
+              </span>
+
+              <p>
+                MongoDB - MongoDB is schema-less, allowing you to change the
+                structure of documents without affecting existing data. This
+                flexibility is well-suited for applications with evolving data
+                requirements.
+              </p>
+              <br />
+              <p>
+                MySQL - MySQL requires a predefined schema, meaning you must
+                define the table structure with specific columns and data types
+                before inserting data. Any changes to the schema may require
+                altering the table, which can be more rigid.
+              </p>
+            </h1>
+            <h1>
+              <span className="text-xl text-[#ff3811]">
+                {" "}
+                3. Query Language:
+              </span>
+
+              <p>
+                MongoDB - MongoDB uses a query language that is based on
+                JavaScript-like syntax. It supports querying documents using
+                JSON-like queries.
+              </p>
+              <br />
+              <p>
+                MySQL - MySQL uses Structured Query Language (SQL), a powerful
+                and standardized language for querying and manipulating
+                relational data. SQL offers complex querying capabilities,
+                including JOIN operations, which are not native to MongoDB.
+              </p>
+            </h1>
+            <h1>
+              <span className="text-xl text-[#ff3811]"> 4. Scaling:</span>
+
+              <p>
+                MongoDB - MongoDB is designed to scale horizontally, which means
+                it can distribute data across multiple servers to handle large
+                amounts of data and high traffic. This makes it well-suited for
+                applications that require horizontal scaling, such as big data
+                and real-time analytics.
+              </p>
+              <br />
+              <p>
+                MySQL - MySQL can be scaled both vertically (by adding more
+                resources to a single server) and horizontally (with solutions
+                like MySQL Cluster or sharding), but horizontal scaling can be
+                more complex to set up and manage.
+              </p>
+            </h1>
+
+            <p>
+              Ultimately, the choice between MongoDB and MySQL depends on the
+              specific requirements of your application, including data
+              structure, scalability needs, and transactional consistency. Some
+              projects even use both databases for different parts of the
+              application, taking advantage of their respective strengths.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
